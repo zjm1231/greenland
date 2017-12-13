049a3168dd7c9d17c8fa92b19d78119ce9b299bb
zjm1231 <jumee.zhang@seatig.com>
2017-12-13 15:45:16 +0800

    first submit

---
+++ Inventory/app/src/main/java/com/greenland/inventory/views/MainActivity.java
@@ -0,0 +1,41 @@
+package com.greenland.inventory.views;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.greenland.inventory.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(getResources().getColor(R.color.color_transparent, getTheme()));
+        }
+
+        RadioGroup rg = (RadioGroup) findViewById(R.id.rdg_login);
+        rg.check(R.id.rdb_login_online);
+
+        Button btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "点击了登录按钮", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
