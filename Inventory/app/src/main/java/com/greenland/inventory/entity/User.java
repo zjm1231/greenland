1e04ce9ee780794360211c8cd83b372156850754
zjm1231 <jumee.zhang@seatig.com>
2017-12-25 10:06:12 +0800

    db

---
+++ Inventory/app/src/main/java/com/greenland/inventory/entity/User.java
@@ -0,0 +1,46 @@
+package com.greenland.inventory.entity;
+
+import org.greenrobot.greendao.annotation.Entity;
+import org.greenrobot.greendao.annotation.Id;
+import org.greenrobot.greendao.annotation.Generated;
+
+/**
+ * Created by jumee on 2017/12/23.
+ */
+
+@Entity
+public class User {
+
+    //不能用int
+    @Id(autoincrement = true)
+    private Long id;
+
+    private  String name;
+
+    @Generated(hash = 873297011)
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Generated(hash = 586692638)
+    public User() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
